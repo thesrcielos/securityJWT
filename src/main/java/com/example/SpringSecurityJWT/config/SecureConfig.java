ea28a4e5d7a224ed43e75458202553244716840b
thesrcielos <diegomadi75@gmail.com>
2024-02-11 22:41:39 -0500

    v2

--- src/main/java/com/example/SpringSecurityJWT/config/SecureConfig.java
+++ src/main/java/com/example/SpringSecurityJWT/config/SecureConfig.java
@@ -2,23 +2,48 @@ package com.example.SpringSecurityJWT.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecureConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.
-        authorizeHttpRequests(request-> request.requestMatchers("/api/**").permitAll()
+        csrf(config->config.disable()).
+        authorizeHttpRequests(request-> request.requestMatchers("/api/hello").permitAll()
                 .anyRequest().authenticated()).
-                formLogin(form->form.permitAll()).
-                sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS).
-                        invalidSessionUrl("/login")).
+                formLogin(AbstractAuthenticationFilterConfigurer::permitAll).
+                sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
+
                 build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("diego").roles().password("1234").build());
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        return auth.build();
     }
 }
--- src/main/java/com/example/SpringSecurityJWT/controller/PrincipalController.java
+++ src/main/java/com/example/SpringSecurityJWT/controller/PrincipalController.java
@@ -1,14 +1,61 @@
 package com.example.SpringSecurityJWT.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.SpringSecurityJWT.model.ERole;
+import com.example.SpringSecurityJWT.model.RoleEntity;
+import com.example.SpringSecurityJWT.model.UserEntity;
+import com.example.SpringSecurityJWT.repository.UserRepository;
+import com.example.SpringSecurityJWT.request.CreateUserDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class PrincipalController {
+    private final UserRepository userRepository;
+
+    public PrincipalController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world not secure";
+    }
+
+    @GetMapping("/helloSecured")
+    public String helloSecured(){
+        return "hello world secured";
+    }
+
+    @PostMapping
+    public String aa(){
+        return "hola";
+
+    }
+    @PostMapping("/createUser")
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserDTO createUserDTO){
+        Set<RoleEntity> roles = createUserDTO.getRoles().stream().
+                map(role -> RoleEntity.builder().name(ERole.valueOf(role)).build()).
+                collect(Collectors.toSet());
+        UserEntity userEntity = UserEntity.builder().
+                username(createUserDTO.getUsername()).
+                password(createUserDTO.getPassword()).
+                email(createUserDTO.getEmail()).
+                roles(roles).
+                build();
+        userRepository.save(userEntity);
+        return ResponseEntity.ok(userEntity);
+
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(@RequestParam String id){
+        userRepository.deleteById(Integer.valueOf(id));
+        return "se ha borrado el user con id".concat(id);
+
     }
 }
